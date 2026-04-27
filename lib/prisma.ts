4029f8c232a5c7936624e65369fd9052cc4c0c8b
AndersonKouadioDev <andersonkouadio0118@gmail.com>
2026-04-27 22:06:08 +0000

    fix:bug prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,24 @@
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+import "server-only"
+
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+  prisma: PrismaClient | undefined
+}
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
-});
+})
 
 const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
-  });
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
 
-export default prisma;
+export default prisma
